52b26f8208315af018b0f0d74ac79902fd1fafb7
oskodiak <oskodiak@proton.me>
2025-11-21 02:33:56 -0800

    Update nvidia.nix

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,14 +1,10 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ 
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
-  # NVIDIA hardware support
   hardware = {
     enableAllFirmware = true;
     enableRedistributableFirmware = true;
+
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = false;
@@ -17,26 +13,31 @@
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
+
     graphics = {
       enable = true;
       enable32Bit = true;
     };
   };
 
-  # NVIDIA drivers
+  # NVIDIA X driver (Wayland via modesetting / nvidia-drm)
   services.xserver.videoDrivers = [ "nvidia" ];
-  
-  # NVIDIA kernel modules
+
   boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-    kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    initrd.availableKernelModules = [
+      "xhci_pci" "ehci_pci" "ahci"
+      "usbhid" "usb_storage" "sd_mod"
+    ];
+
+    kernelModules = [
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+    ];
+
     kernelParams = [
       "nvidia-drm.modeset=1"
     ];
   };
-
-  # Filesystem configuration
-
-  swapDevices = lib.mkDefault [ ];
-  networking.useDHCP = lib.mkDefault true;
 }
